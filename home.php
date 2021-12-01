9b26a6e409abbf317fe3a3cbfd147c622f974fea
Junior <junior.ng@outlook.com.br>
2021-12-01 20:34:29 -0300

    Feat: add home

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+    die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    Bem vindo à página, <?php echo $_SESSION['nome']; ?>.
+
+</body>
+</html>
