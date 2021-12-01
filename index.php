224eb9d59f7b76ea9190be82f57b6fca9292a481
Junior <junior.ng@outlook.com.br>
2021-12-01 20:32:45 -0300

    Feat: login

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
 include('conexao.php');
+
+if(isset($_POST['nome']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['nome']) == 0) {
+        echo "Informe seu nome";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Informe sua senha";
+    } else {
+
+        $nome = $mysqli->real_escape_string($_POST['nome']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios where nome = '$nome' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("location: home.php");
+            
+        } else {
+            echo"Falha ao se conectar ! nome ou senha incorretos";
+        }
+        
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
