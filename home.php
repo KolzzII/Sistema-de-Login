5f468946afbe119c32baad23bbb03a104a076e30
Junior <junior.ng@outlook.com.br>
2021-12-08 20:57:23 -0300

    Style: add estilo

--- home.php
+++ home.php
@@ -16,12 +16,11 @@ if(!isset($_SESSION['id'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    Bem vindo à página, <?php echo $_SESSION['nome']; ?>.
+    <h3>Bem vindo à página, <?php echo $_SESSION['nome']; ?>.</h3>
 
-    <p>
-        <a href="logout.php">Sair</a>
-    </p>
+    <a href="logout.php"><p>Sair</p></a>
 </body>
 </html>
